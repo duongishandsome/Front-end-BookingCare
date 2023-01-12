457d581bb29e334f20f7ae513bf0a498832aeee7
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-13 01:50:48 +0700

    CRUD vs ReactJs 2

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,4 +4,8 @@ const handleLoginApi = (userEmail, userPassword) => {
     return axios.post('/api/login', { email: userEmail, password: userPassword });
 };
 
-export default handleLoginApi;
+const getAllUsers = (inputId) => {
+    return axios.get(`api/get-all-users?id=${inputId}`);
+};
+
+export { handleLoginApi, getAllUsers };
