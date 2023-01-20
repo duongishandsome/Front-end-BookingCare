aa85da794e429c827114d152c963d8d8b291f82b
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-20 13:11:07 +0700

    su dung redux trong du an React Phan 1

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,6 +3,7 @@ import { connectRouter } from 'connected-react-router';
 
 import appReducer from './appReducer';
 import userReducer from './userReducer';
+import adminReducer from './adminReducer'
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
@@ -25,9 +26,11 @@ const appPersisConfig = {
     whitelist: ['language'],
 };
 
+// eslint-disable-next-line
 export default (history) =>
     combineReducers({
         router: connectRouter(history),
         user: persistReducer(userPersistConfig, userReducer),
         app: persistReducer(appPersisConfig, appReducer),
+        admin: adminReducer
     });
