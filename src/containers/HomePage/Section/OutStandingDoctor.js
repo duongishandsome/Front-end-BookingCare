e0bb3fcf65e718d8dc9c0594ac20e827bc5b4d64
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-27 23:40:47 +0700

    Trang xem chi tiet doctor

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -5,6 +5,8 @@ import { LANGUAGES } from '../../../utils';
 
 import Slider from 'react-slick';
 import { FormattedMessage } from 'react-intl';
+import { Redirect } from 'react-router';
+import { withRouter } from 'react-router';
 
 class OutStandingDoctor extends Component {
     constructor(props) {
@@ -24,6 +26,13 @@ class OutStandingDoctor extends Component {
     componentDidMount() {
         this.props.loadTopDoctors();
     }
+
+    handleViewDetailDoctor = (doctor) => {
+        if (this.props.history) {
+            this.props.history.push(`/detail-doctor/${doctor.id}`);
+        }
+    };
+
     render() {
         let arrDoctors = this.state.arrDoctors;
         let { language } = this.props;
@@ -50,7 +59,11 @@ class OutStandingDoctor extends Component {
                                     let nameVi = `${item.positionData.valueVi}, ${item.lastName} ${item.firstName}`;
                                     let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
                                     return (
-                                        <div className="section-customize" key={index}>
+                                        <div
+                                            className="section-customize"
+                                            key={index}
+                                            onClick={() => this.handleViewDetailDoctor(item)}
+                                        >
                                             <div className="customize-border">
                                                 <div className="outer-bg">
                                                     <div
@@ -88,4 +101,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor));
--- src/services/userService.js
+++ src/services/userService.js
@@ -40,6 +40,10 @@ const saveDetailDoctorService = (data) => {
     return axios.post('/api/save-info-doctors', data);
 };
 
+const getDetailInforDoctor = (id) => {
+    return axios.get(`/api/get-detail-doctor-by-id?id=${id}`);
+};
+
 export {
     handleLoginApi,
     getAllUsers,
@@ -50,4 +54,5 @@ export {
     getTopDoctorHomeService,
     getAllDoctors,
     saveDetailDoctorService,
+    getDetailInforDoctor,
 };
