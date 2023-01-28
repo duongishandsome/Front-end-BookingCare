77b1492810cbf0eb31e60a7b4495614f2710c9d3
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-01-28 17:38:10 +0700

    Tạo Menu Phân Quyền Người Dùng Khi Đăng Nhập

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -19,9 +19,15 @@ export const adminMenu = [
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ],
             },
+            // {
+            //     name: 'menu.admin.manage-admin',
+            //     link: '/system/user-admin',
+            // },
             {
-                name: 'menu.admin.manage-admin',
-                link: '/system/user-admin',
+                //quản lý kế hoạch khám bệnh của bác sĩ
+
+                name: 'menu.doctor.manage-schedule',
+                link: '/doctor/manage-schedule',
             },
         ],
     },
@@ -52,6 +58,19 @@ export const adminMenu = [
             {
                 name: 'menu.admin.manage-handbook',
                 link: '/system/manage-handbook',
+            },
+        ],
+    },
+];
+
+export const doctorMenu = [
+    {
+        name: 'menu.admin.manage-user',
+        menus: [
+            {
+                //quản lý kế hoạch khám bệnh của bác sĩ
+
+                name: 'menu.doctor.manage-schedule',
             },
         ],
     },
