6fb059d291a90928015691c26738dc0f8d5322f1
Duong Hoang <hvdvietnamquetoi@gmail.com>
2023-02-11 22:17:29 +0700

    Hoàn ThiệnXem Chi Tiết Chyên Khoa Khám

--- src/containers/Patient/Doctor/ProfileDoctor.js
+++ src/containers/Patient/Doctor/ProfileDoctor.js
@@ -8,6 +8,7 @@ import { getProfileDoctorById } from '../../../services/userService';
 import NumberFormat from 'react-number-format';
 import _ from 'lodash';
 import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 class ProfileDoctor extends Component {
     constructor(props) {
@@ -68,7 +69,7 @@ class ProfileDoctor extends Component {
 
     render() {
         let { dataProfile } = this.state;
-        let { language, isShowDescriptionDoctor, dataTime } = this.props;
+        let { language, isShowDescriptionDoctor, dataTime, iShowLinkDetail, isShowPrice, doctorId } = this.props;
         let nameVi = '',
             nameEn = '';
         if (dataProfile && dataProfile.positionData) {
@@ -97,27 +98,35 @@ class ProfileDoctor extends Component {
                         </div>
                     </div>
                 </div>
-                <div className="price">
-                    <FormattedMessage id="patient.booking-modal.price" />
-                    {dataProfile && dataProfile.Doctor_Infor && language === LANGUAGES.VI && (
-                        <NumberFormat
-                            className="currency"
-                            value={dataProfile.Doctor_Infor.priceTypeData.valueVi}
-                            displayType={'text'}
-                            thousandSeparator={true}
-                            suffix={'VND'}
-                        />
-                    )}
-                    {dataProfile && dataProfile.Doctor_Infor && language === LANGUAGES.EN && (
-                        <NumberFormat
-                            className="currency"
-                            value={dataProfile.Doctor_Infor.priceTypeData.valueEn}
-                            displayType={'text'}
-                            thousandSeparator={true}
-                            suffix={'USD'}
-                        />
-                    )}
-                </div>
+                {iShowLinkDetail === true && (
+                    <div className="view-detail-doctor">
+                        <Link to={`/detail-doctor/${doctorId}`}>Xem thêm</Link>
+                    </div>
+                )}
+
+                {isShowPrice === true && (
+                    <div className="price">
+                        <FormattedMessage id="patient.booking-modal.price" />
+                        {dataProfile && dataProfile.Doctor_Infor && language === LANGUAGES.VI && (
+                            <NumberFormat
+                                className="currency"
+                                value={dataProfile.Doctor_Infor.priceTypeData.valueVi}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                                suffix={'VND'}
+                            />
+                        )}
+                        {dataProfile && dataProfile.Doctor_Infor && language === LANGUAGES.EN && (
+                            <NumberFormat
+                                className="currency"
+                                value={dataProfile.Doctor_Infor.priceTypeData.valueEn}
+                                displayType={'text'}
+                                thousandSeparator={true}
+                                suffix={'USD'}
+                            />
+                        )}
+                    </div>
+                )}
             </div>
         );
     }
--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -45,10 +45,22 @@ class DetailSpecialty extends Component {
                         });
                     }
                 }
+                let dataProvince = resProvince.data;
+                let result = {};
+                if (dataProvince && dataProvince.length > 0) {
+                    dataProvince.unshift({
+                        createAt: null,
+                        myMap: 'ALL',
+                        type: 'PROVINCE',
+                        valueEn: 'ALL',
+                        valueVi: 'Toàn quốc',
+                    });
+                }
+
                 this.setState({
                     dataDetailSpecialty: res.data,
                     arrDoctorId: arrDoctorId,
-                    listProvince: resProvince.data,
+                    listProvince: dataProvince ? dataProvince : [],
                 });
             }
         }
@@ -56,9 +68,36 @@ class DetailSpecialty extends Component {
 
     async componentDidUpdate(prevProps, prevState, snapshot) {}
 
-    handleOnChangeSelect = (e) => {
-        console.log('check onChange', e.target.value);
+    handleOnChangeSelect = async (e) => {
+        if (this.props.match && this.props.match.params && this.props.match.params.id) {
+            let id = this.props.match.params.id;
+            let location = e.target.value;
+
+            let res = await getDetailSpecialtyById({
+                id,
+                location: location,
+            });
+
+            if (res && res.errCode === 0) {
+                let data = res.data;
+                let arrDoctorId = [];
+
+                if (data && !_.isEmpty(data)) {
+                    let arr = data.doctorSpecialty;
+                    if (arr && arr.length > 0) {
+                        arr.map((item) => {
+                            arrDoctorId.push(item.doctorId);
+                        });
+                    }
+                }
+                this.setState({
+                    dataDetailSpecialty: res.data,
+                    arrDoctorId: arrDoctorId,
+                });
+            }
+        }
     };
+
     render() {
         let { arrDoctorId, dataDetailSpecialty, listProvince } = this.state;
         let { language } = this.props;
@@ -72,40 +111,47 @@ class DetailSpecialty extends Component {
                             <div dangerouslySetInnerHTML={{ __html: dataDetailSpecialty.descriptionHTML }}></div>
                         )}
                     </div>
-                    <div className="search-sp-doctor">
-                        <select onChange={(e) => this.handleOnChangeSelect(e)}>
-                            {listProvince &&
-                                listProvince.length > 0 &&
-                                listProvince.map((item, index) => {
-                                    return (
-                                        <option key={index} value={item.keyMap}>
-                                            {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
-                                        </option>
-                                    );
-                                })}
-                        </select>
+                    <div className="content-specialty">
+                        <div className="search-sp-doctor">
+                            <select onChange={(e) => this.handleOnChangeSelect(e)}>
+                                {listProvince &&
+                                    listProvince.length > 0 &&
+                                    listProvince.map((item, index) => {
+                                        return (
+                                            <option key={index} value={item.keyMap}>
+                                                {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                            </option>
+                                        );
+                                    })}
+                            </select>
+                        </div>
+                        {arrDoctorId &&
+                            arrDoctorId.length > 0 &&
+                            arrDoctorId.map((item, index) => {
+                                return (
+                                    <div className="each-doctor" key={index}>
+                                        <div className="dt-content-left">
+                                            <div className="profile-doctor">
+                                                <ProfileDoctor
+                                                    doctorId={item}
+                                                    isShowDescriptionDoctor={true}
+                                                    iShowLinkDetail={true}
+                                                    isShowPrice={false}
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="dt-content-right">
+                                            <div className="doctor-schedule">
+                                                <DoctorSchedule doctorIdFromParent={item} />
+                                            </div>
+                                            <div className="doctor-extra-infor">
+                                                <DoctorExtraInfor doctorIdFromParent={item} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
                     </div>
-                    {arrDoctorId &&
-                        arrDoctorId.length > 0 &&
-                        arrDoctorId.map((item, index) => {
-                            return (
-                                <div className="each-doctor" key={index}>
-                                    <div className="dt-content-left">
-                                        <div className="profile-doctor">
-                                            <ProfileDoctor doctorId={item} isShowDescriptionDoctor={true} />
-                                        </div>
-                                    </div>
-                                    <div className="dt-content-right">
-                                        <div className="doctor-schedule">
-                                            <DoctorSchedule doctorIdFromParent={item} />
-                                        </div>
-                                        <div className="doctor-extra-infor">
-                                            <DoctorExtraInfor doctorIdFromParent={item} />
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                        })}
                 </div>
             </>
         );
